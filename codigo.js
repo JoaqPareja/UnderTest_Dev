98815b587d3483465c0c0eab7ed29e94b58a2e66
JoaqPareja <80791761+JoaqPareja@users.noreply.github.com>
2021-10-24 01:53:13 -0300

    change madrugada

---
+++ codigo.js
@@ -0,0 +1,16 @@
+let rotateAngle = 360;
+
+ function rotate(image) {
+    image.setAttribute("style", "transform: rotate(" + rotateAngle + "deg)");
+  //rotateAngle = rotateAngle;
+  
+}
+
+
+
+/*window.onload= function rotate(image) {
+  image.setAttribute("style", "transform: rotate(" + rotateAngle + "deg)");
+  rotateAngle = rotateAngle;
+}*/
+
+//window.onload = load;
